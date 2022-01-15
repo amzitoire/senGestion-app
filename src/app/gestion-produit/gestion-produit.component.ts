1ca4b0d0fcb1ca22b173c60d769252e2287c01ff
amzitoire <amzidou16@gmail.com>
2022-01-15 18:24:18 +0000

    recherche bug update

--- src/app/gestion-produit/gestion-produit.component.ts
+++ src/app/gestion-produit/gestion-produit.component.ts
@@ -11,9 +11,11 @@ import { ProduitService } from '../produit.service';
 })
 
 export class GestionProduitComponent implements OnInit {
- search:Array<Produit> = [];
-  
+
+  search:Array<Produit> = [];
   produits:Array<Produit> = [];
+  
+
   detailsProduit: any;
   editProduit: any;
 
@@ -22,6 +24,7 @@ export class GestionProduitComponent implements OnInit {
   handleSearch(myForm: NgForm) {
    
    this.search= this.produitService.search(myForm.value.search);
+   
     if (this.search.length) {
        this.produits = this.search;
     }
@@ -34,30 +37,36 @@ export class GestionProduitComponent implements OnInit {
      if (confirm("Voulez vous vraiment supprimer  le produit ? ") == true) {
       this.produitService.deleteProduit(produit);
       this.produitService.deleteProduitsJson(produit.id).subscribe();
-    } 
+    }
+    
+    this.produits =this.produitService.getAll();
     }
   ajouterproduit(produit: any) {
+
+    this.produits= this.produitService.getAll();
+
     var id =1;
     for (let index = 0; index < this.produits.length; index++) {
       const element =this.produits[index];
-      if (element.getId >id){
+      if (element.getId >=id){
         id=element.getId+1
       }
     };
 
-      let a = new Produit( id,produit.nom,produit.prix,produit.description);
+    let a = new Produit( id,produit.nom,produit.prix,produit.description);
       
-      this.produitService.addProduit(a);
-      this.produitService.addProduitJson(a).subscribe();
+    this.produitService.addProduit(a);
+    this.produitService.addProduitJson(a).subscribe();      
     }
   modifierProduit(produit: any){ 
     let a = new Produit(  this.produitService.getProduit(this.editProduit).getId,produit.nomEdit,produit.prixEdit,produit.descriptionEdit);
     this.produitService.editProduit(this.produitService.getProduit(this.editProduit),a);
     this.produitService.updateJson(a,a.getId).subscribe();
+
     this.detailsProduit=null;
     this.editProduit = null;
-   
-    
+    this.produits =this.produitService.getAll();
+
   }
   cancelModifierProduit(produit: any){ 
     this.detailsProduit=null;
